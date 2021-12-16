28650e69f0981619bc9a1fa0a323c45796e25871
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2021-12-15 16:40:05 -0800

    Add method override

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var methodOverride = require('method-override');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -26,6 +28,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(methodOverride('_method'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
